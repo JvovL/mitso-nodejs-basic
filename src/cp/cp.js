c69d195540a7deacb6b98061bc07afc9009c96e6
JvovL <jjvovo715@gmail.com>
2025-02-16 21:49:58 +0300

    dir cp compl

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,28 +1,21 @@
-import * as fs from "node:fs/promises";
+import { spawn } from "child_process";
+import { fileURLToPath } from "url";
 import path from "path";
-const copy = async () => {
-  try {
-    await fs.mkdir(path.join("src/fs/files_copy"), { recursive: true });
-    console.log("папка успешно создана");
 
-    const files = await fs.readdir(path.join("src/fs/files"), {
-      withFileTypes: true,
-    });
-    files.forEach(async (file) => {
-      if (file.isFile()) {
-        await fs.copyFile(
-          path.join("src/fs/files", file.name),
-          path.join("src/fs/files_copy", file.name)
-        );
-      }
-    });
-  } catch (error) {
-    if (error.code === "EEXIST" || error.code === "ENOENT") {
-      console.log("FS operation failed");
-    } else {
-      console.log("Произошла ошибка:", error);
-    }
-  }
-};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-await copy();
+function spawnChildProcess(args) {
+  const scriptPath = path.join(__dirname, "./files/script.js");
+  const child = spawn("node", [scriptPath, ...args]);
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
+  child.stderr.pipe(process.stderr);
+
+  child.on("close", (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
+}
+
+spawnChildProcess(process.argv.slice(2));
